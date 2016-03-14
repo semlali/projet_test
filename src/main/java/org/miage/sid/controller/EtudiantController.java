53c14f24b23b5e037b96007637b6416fbbdfaae5
samia <semlali.samia@gmail.com>
2016-03-14 15:29:07 +0000

    update controller

--- src/main/java/org/miage/sid/controller/EtudiantController.java
+++ src/main/java/org/miage/sid/controller/EtudiantController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 //ajouter header
+//modificer le controller
 
 @Controller
 public class EtudiantController {
